3fc8789eba01f8567ab13301cf1c60c7d22b6464
hal-efecan <hefecan@gmail.com>
2020-02-29 12:49:37 +0000

    code refactor

--- src/views/LogIn.js
+++ src/views/LogIn.js
@@ -3,12 +3,13 @@ import React, { useEffect } from 'react';
 function LogIn() {
 
     function handleServerRedirect() {
-        let backend_uri = process.env.BACKEND_URI
+        // let backend_uri = process.env.BACKEND_URI
         if(process.env.NODE_ENV !== 'production') {
-          window.location.href = `https://hidden-depths-47482.herokuapp.com/login`
-            // window.location.href  = `http://localhost:8888/login`
+          // window.location.href = `https://hidden-depths-47482.herokuapp.com/login`
+            window.location.href  = `http://localhost:8888/login`
         } else {
-            window.location.href  = backend_uri 
+            // window.location.href  = backend_uri 
+            window.location.href = `https://hidden-depths-47482.herokuapp.com/login`
         }
       }
       
