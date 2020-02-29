30074de1b25f980cae866803ee4d350097596998
hal-efecan <hefecan@gmail.com>
2020-02-29 13:51:36 +0000

    updated loader css

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Loading() {
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignContent: 'center', marginLeft: '275px'}}>
-            <h1>Loading...</h1>
+            {/* <h1>Loading...</h1> */}
+            <div className="spinner">
+            </div>
+            
         </div>
     )
 }
